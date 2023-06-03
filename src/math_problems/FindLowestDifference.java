9af186b152b63b38db48dcef286455672237b63d
layla1307 <layla1307@gmail.com>
2023-06-03 09:47:32 -0400

    Initial Commit

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -7,11 +7,25 @@ public class FindLowestDifference {
      * HINT: The lowest number that isn't shared between these arrays is 1
      */
 
+
+    static int lowestDifference(int[] array, int n)
+    {
+        int difference = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n-1; i++)
+            for (int j = i + 1; j < n; j++)
+                if (Math.abs((array[i]) )< difference)
+                    difference = Math.abs((array[i] - array[j]));
+        return difference;
+    }
     public static void main(String[] args) {
 
 
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
+
+            System.out.println("the minimum difference between array1 and array2 is " + lowestDifference(array1, array1.length));
+
 
     }
 
