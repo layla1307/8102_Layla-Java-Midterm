7d6ada0f85f55748d2f3d239971fd591820ca009
layla1307 <layla1307@gmail.com>
2023-06-03 09:32:07 -0400

    Initial Commit

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -8,7 +8,22 @@ public class Fibonacci {
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
+
+    static void printFibonacciNumber(int f){
+        int num1=0;
+        int num2=1;
+        System.out.println(num1 + "\n" + num2);
+        for (int i=3; i<=f;i++){
+            int nextNum = num1+num2;
+            System.out.println(nextNum + " ");
+            num1 = num2;
+            num2 = nextNum;
+        }
+    }
     public static void main(String[] args) {
+        int f = 40;
+        System.out.println("Here are the " + f + " Fibonacci numbers :");
+        printFibonacciNumber(f);
 
     }
 }
