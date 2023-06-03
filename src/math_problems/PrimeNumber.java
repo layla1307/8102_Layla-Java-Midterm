982230ca510014bcce71489aaac006a42991e737
layla1307 <layla1307@gmail.com>
2023-06-03 11:32:25 -0400

    Initial Commit

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -10,7 +10,29 @@ public class PrimeNumber {
      */
 
 
+    public static void printPrimeNumbers(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
     public static void main(String[] args) {
+        int n = 1000000;
+        printPrimeNumbers(n);
 
     }
 
